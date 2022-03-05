5dee6abc476fa420f67b4cb53c03221d93235c7f
Rowan Manning <accounts+github@rowanmanning.com>
2022-03-05 12:28:19 +0000

    Add integration tests

---
+++ test/integration/express4.test.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const {assert} = require('chai');
+const createTestApp = require('./fixture/create-test-app');
+
+describe('Express 4', () => {
+	let app;
+
+	before(async () => {
+		app = await createTestApp('express4');
+	});
+
+	after(() => {
+		app.stop();
+	});
+
+	describe('GET /404-no-message', () => {
+		let response;
+
+		beforeEach(async () => {
+			response = await app.get('/404-no-message');
+		});
+
+		it('responds with a 404 status', () => {
+			assert.strictEqual(response.status, 404);
+		});
+
+		it('responds with the expected message', () => {
+			assert.include(response.data, 'NotFoundError: Not Found');
+		});
+
+	});
+
+	describe('GET /404-with-message', () => {
+		let response;
+
+		beforeEach(async () => {
+			response = await app.get('/404-with-message');
+		});
+
+		it('responds with a 404 status', () => {
+			assert.strictEqual(response.status, 404);
+		});
+
+		it('responds with the expected message', () => {
+			assert.include(response.data, 'NotFoundError: Nope');
+		});
+
+	});
+
+});
